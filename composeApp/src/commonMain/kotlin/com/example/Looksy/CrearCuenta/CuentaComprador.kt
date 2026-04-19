f4d5308a32a37958fad452eeea3779a7a9b7a4e2
dafneg12 <s23120167@alumnos.itsur.edu.mx>
2026-04-18 21:27:51 -0600

    Avance de pantalla crear cliente

--- composeApp/src/commonMain/kotlin/com/example/Looksy/CrearCuenta/CuentaComprador.kt
+++ composeApp/src/commonMain/kotlin/com/example/Looksy/CrearCuenta/CuentaComprador.kt
@@ -2,17 +2,39 @@ package com.example.Looksy.CrearCuenta
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CuentaComprador(
-    onVolver: () -> Unit
+    onVolver: () -> Unit,
+    onGuardar: () -> Unit = {}
 ) {
+    var usuario by remember { mutableStateOf("") }
+    var contrasena by remember { mutableStateOf("") }
+    var mostrarContrasena by remember { mutableStateOf(false) }
+
+    var nombre by remember { mutableStateOf("") }
+    var apellidoP by remember{ mutableStateOf("") }
+    var apellidoM by remember{ mutableStateOf("") }
+
+
+    var correo by remember { mutableStateOf("") }
+    var direccion by remember { mutableStateOf("") }
+    var error by remember { mutableStateOf<String?>(null) }
+
+    var rol by remember { mutableStateOf("cliente") }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -24,6 +46,120 @@ fun CuentaComprador(
             Text("Volver")
         }
 
+        OutlinedTextField(
+            value = usuario,
+            onValueChange = { usuario = it },
+            label = { Text("Usuario") },
+            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+        )
 
+        OutlinedTextField(
+            value = contrasena,
+            onValueChange = { contrasena = it },
+            label = { Text("Contraseña") },
+            visualTransformation = if (mostrarContrasena)
+                VisualTransformation.None
+            else
+                PasswordVisualTransformation(),
+            trailingIcon = {
+                val icon = if (mostrarContrasena)
+                    Icons.Default.Visibility
+                else
+                    Icons.Default.VisibilityOff
+
+                IconButton(onClick = {
+                    mostrarContrasena = !mostrarContrasena
+                }) {
+                    Icon(imageVector = icon, contentDescription = "Mostrar contraseña")
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 12.dp)
+        )
+
+        OutlinedTextField(
+            value = nombre,
+            onValueChange = { nombre = it },
+            label = { Text("Nombre del usuario") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 12.dp)
+        )
+
+        OutlinedTextField(
+            value = apellidoP,
+            onValueChange = { apellidoP = it },
+            label = { Text("Apellido Paterno") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 12.dp)
+        )
+
+        OutlinedTextField(
+            value = apellidoM,
+            onValueChange = { apellidoM = it },
+            label = { Text("Apellido Materno") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 12.dp)
+        )
+
+
+
+        OutlinedTextField(
+            value = correo,
+            onValueChange = { correo = it },
+            label = { Text("Correo electrónico") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 12.dp)
+        )
+
+        OutlinedTextField(
+            value = direccion,
+            onValueChange = { direccion = it },
+            label = { Text("Dirección de la tienda") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 12.dp)
+        )
+
+        Button(
+            onClick = {
+                when {
+                            usuario.isEmpty() ||
+                            nombre.isEmpty() || apellidoP.isEmpty() || apellidoM.isEmpty() ||
+                            correo.isEmpty() || direccion.isEmpty() ||
+                            contrasena.isEmpty() -> {
+                        error = "Todos los campos son obligatorios"
+                    }
+
+                    !correo.contains("@") -> {
+                        error = "Ingresa un correo valido"
+                    }
+
+                    else -> {
+                        error = null
+                        println("Registro tienda: $usuario - $nombre")
+                        onVolver() // regresar después de registrar
+                    }
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        ) {
+            Text("Registrar")
+        }
+
+        if (error != null) {
+            Text(
+                text = error!!,
+                color = MaterialTheme.colorScheme.error,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
     }
 }
